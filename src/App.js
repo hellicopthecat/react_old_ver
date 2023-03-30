0dbd3112b84ca9d7f980a8230508d878164cebe0
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-03-30 16:14:07 +0900

    component practice_part_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food(props) {
+  return <h1>I'm a {props.more}</h1>;
+}
+function Drinks({what}) {
+  return <h1>I'm a {what}</h1>;
+}
+function Whatmatch({match, whatdrink}) {
+  return (
+    <div>
+      <h1>
+        {match} && {whatdrink}
+      </h1>
+    </div>
+  );
+}
+const foodList = [
+  {name: "chicken", with: "beer"},
+  {name: "row Fish", with: "soju"},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food more="cheese" />
+      <Drinks what="coffee" />
+      {foodList.map((current) => (
+        <Whatmatch match={current.name} whatdrink={current.with} />
+      ))}
     </div>
   );
 }
